72d4fa53db063222a86a77e51877ab60aa8bae6b
Pepe Becker <mail@pepebecker.com>
2018-02-18 15:50:11 +0100

    Update modules

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const BOT_TOKEN = 'SECRET'
 
 const bot = new Telegraf(BOT_TOKEN)
 
-bot.command('start', (ctx) => {
-	const text = 'nǐ hǎo'
+bot.command('start', ctx => {
+	console.log('Bot started')
+	const text = '你好'
 	ctx.reply(text)
-	console.log('Sent message:', text)
 })
 
-bot.on('text', (ctx) => {
+bot.on('text', ctx => {
 	const text = ctx.update.message.text
-	botCore.processMessage(text, ctx.reply)
-	console.log('Sent message:', text)
+	console.log('Received message:', text)
+	botCore.processMessage(text)
+	.then(ctx.reply)
+	.catch(console.error)
 })
 
-bot.catch((err) => {
+bot.catch(err => {
 	console.error('Ooops', err)
 })
 
